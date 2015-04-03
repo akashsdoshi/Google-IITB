54e44910e0acbef9832eb5576b423f3f5531d676
Aditya Bhosale <140010009@iitb.ac.in>
2015-04-03 16:34:28 +0530

    remove Search

--- Desktop/project_final/introduceSpellCheck/search/setup.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.system("wget 'https://bootstrap.pypa.io/get-pip.py'")
-#uid = os.getlogin()
-#os.system("cd /home/" + uid + "/Downloads")
-os.system("sudo python get-pip.py")
-os.system("sudo pip install dropbox")
-os.system("echo 'Use \"python main.py\" for running the search engine'")
