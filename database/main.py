fc71374e25f64d09d3bfc3d35d73fbd1fb43669c
Aditya Bhosale <140010009@iitb.ac.in>
2015-04-17 16:48:08 +0530

    remove

--- database/main.py
+++
@@ -1,8 +0,0 @@
-from functions import hashTable
-from functions import uploadToCloud
-
-code = raw_input("Authorization code: ")
-hashTable("NGramkeys.txt")
-
-uploadhashTableToCloud(code)	#also upload to dropbox
-
